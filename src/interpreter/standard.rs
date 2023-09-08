7729b09f0911768783cd5c388729e1602afef7b5
RechieKho <the.real.richie.kho@gmail.com>
2023-09-08 20:41:34 +0800

    Add `assert_argument`.

--- src/interpreter/standard.rs
+++ src/interpreter/standard.rs
@@ -2,6 +2,24 @@ use crate::parser::command::Atom;
 
 use super::{object::Object, value::Value};
 
+macro_rules! assert_argument {
+    ($argument:expr; is $type:expr) => {
+        match $argument {
+            Value::$type(..) => {}
+            _ => panic!("Argument is not the type '{}'.", stringify($type)),
+        }
+    };
+
+    ($argument:expr; in $range:expr) => {
+        if !$range.contains($argument.len()) {
+            panic!(
+                "The argument count is not between {} to {}.",
+                $range.start, $range.end
+            );
+        }
+    };
+}
+
 pub fn greet<'code>(_context: &mut Object, _body: &[Atom<'code>]) -> Value<'code> {
     println!("Hello world");
     Value::NULL

ceaac105c6d3e8c7660b1a4187e4230e818d7263
RechieKho <the.real.richie.kho@gmail.com>
2023-09-09 19:51:02 +0800

    Fix `assert_argument` macros.

--- src/interpreter/standard.rs
+++ src/interpreter/standard.rs
@@ -3,15 +3,12 @@ use crate::parser::command::Atom;
 use super::{object::Object, value::Value};
 
 macro_rules! assert_argument {
-    ($argument:expr; is $type:expr) => {
-        match $argument {
-            Value::$type(..) => {}
-            _ => panic!("Argument is not the type '{}'.", stringify($type)),
-        }
+    ($argument:expr; is $type:pat) => {
+        if !matches!($argument, $type) { panic!("The argument doesn't match the type '{}'.", stringify!($type)); }
     };
 
     ($argument:expr; in $range:expr) => {
-        if !$range.contains($argument.len()) {
+        if !$range.contains(&$argument.len()) {
             panic!(
                 "The argument count is not between {} to {}.",
                 $range.start, $range.end
@@ -22,5 +19,7 @@ macro_rules! assert_argument {
 
 pub fn greet<'code>(_context: &mut Object, _body: &[Atom<'code>]) -> Value<'code> {
     println!("Hello world");
+    assert_argument!(_body; in 2..3);
+    //assert_argument!(_body[1]; is Atom::STRING(_, _));
     Value::NULL
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod parser;
 use interpreter::object::Object;
 
 fn main() {
-    let code = String::from("greet");
+    let code = String::from("greet you");
     let mut context = Object::default();
     context.run_code(&code);
 }

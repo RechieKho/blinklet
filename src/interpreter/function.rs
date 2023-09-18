a8b05541a389fb0ce000d2644e09d1f397245a89
RechieKho <the.real.richie.kho@gmail.com>
2023-09-18 11:14:33 +0800

    Implement `ScriptFunction::call`.

--- src/interpreter/function.rs
+++ src/interpreter/function.rs
@@ -1,7 +1,12 @@
 use super::object::Object;
 use super::value::Value;
 use crate::parser::command::Atom;
+use crate::parser::command::AtomValue;
 use std::sync::Arc;
+
+pub const RETURN_KEY: &'static str = "return";
+pub const RESULT_KEY: &'static str = "result";
+pub const ERROR_KEY: &'static str = "error";
 
 pub trait Function<'code>: ToString {
     fn call(&self, context: &mut Object<'code>, body: &[Atom<'code>]);
@@ -24,8 +29,72 @@ impl<'code> ToString for ScriptFunction<'code> {
 }
 
 impl<'code> Function<'code> for ScriptFunction<'code> {
-    fn call(&self, _context: &mut Object<'code>, _body: &[Atom<'code>]) {
-        // TODO: Implement this.
+    fn call(&self, context: &mut Object<'code>, body: &[Atom<'code>]) {
+        let mut argument_index = 1usize;
+        for atom in self.command.iter() {
+            match atom.value {
+                AtomValue::IDENTIFIER(parameter_identifier) => {
+                    if argument_index >= body.len() {
+                        continue;
+                    }
+                    let argument_atom = &body[argument_index];
+                    let argument = match argument_atom.value {
+                        AtomValue::IDENTIFIER(argument_identifier) => context
+                            .content
+                            .get(argument_identifier)
+                            .unwrap_or(&Value::NULL)
+                            .clone(),
+                        AtomValue::BOOL(boolean) => Value::BOOL(boolean),
+                        AtomValue::NULL => Value::NULL,
+                        AtomValue::STRING(string) => Value::STRING(String::from(string)),
+                        AtomValue::NUMBER(number) => Value::NUMBER(number),
+                        AtomValue::COMMAND(ref command) => {
+                            context.push(Object::default());
+                            let result = context.run_command(command.as_slice());
+                            let mut object = context.pop().unwrap();
+                            if result.is_err() {
+                                context.content.insert(
+                                    String::from(ERROR_KEY),
+                                    Value::STRING(String::from(result.unwrap_err().message)),
+                                );
+                                return;
+                            }
+                            if object.content.contains_key(RESULT_KEY) {
+                                object.content.remove(RESULT_KEY).unwrap_or(Value::NULL)
+                            } else {
+                                Value::NULL
+                            }
+                        }
+                    };
+                    context
+                        .content
+                        .insert(String::from(parameter_identifier), argument);
+                    argument_index += 1;
+                }
+
+                AtomValue::COMMAND(ref command) => {
+                    let result = context.run_command(command);
+                    if result.is_err() {
+                        context.content.insert(
+                            String::from(ERROR_KEY),
+                            Value::STRING(String::from(result.unwrap_err().message)),
+                        );
+                        return;
+                    }
+                    if context.content.contains_key(RETURN_KEY) {
+                        return;
+                    }
+                }
+
+                _ => {
+                    context.content.insert(
+                        String::from(ERROR_KEY),
+                        Value::STRING(String::from("Unexpected value as the head of a command.")),
+                    );
+                    return;
+                }
+            }
+        }
     }
 }
 
